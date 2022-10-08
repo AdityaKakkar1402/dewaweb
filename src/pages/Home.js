a41720bbfd407a82c8d9f0c5fec12088913f441c
AdityaKakkar1402 <kakkar.adi14@gmail.com>
2022-10-08 15:23:30 +0530

    jj

---
+++ src/pages/Home.js
@@ -0,0 +1,49 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div>
+            <div className="bgimg">
+                <img className="bgimage" src="backgroundhome.png" alt="" srcset="" />
+            </div>
+            <div className="csimg">
+              <div className="csimagecont"><img className="csimage" id="img1"src="cs2.png" alt="" srcset="" /></div>
+              <div className="csimagecont"><img className="csimage" id="img2" src="cs1.png" alt="" srcset="" /></div>
+              <div className="csimagecont"> <img className="csimage" id="img3" src="cs3.png" alt="" srcset="" /></div>
+                
+                
+               
+            </div>
+            <div className="homecontent">
+                <div className="dewatextcont">
+                <div className="textimgcont">
+                <img className="textimg" src="dewatext2.png" alt="" srcset="" />
+                </div>
+                
+                <h1 className="dewades">DECENTRALISED WATCH</h1>
+
+                <div className="watchimgcont">
+                <img className="watchimg" src="watchimg.png" alt="" srcset="" />
+                </div>
+
+
+                </div>
+                
+                
+            </div>
+
+
+
+            <div className="csimg2">
+              <div className="csimagecont"><img className="csimage" id="img1"src="cs2.png" alt="" srcset="" /></div>
+              <div className="csimagecont"><img className="csimage" id="img2" src="cs1.png" alt="" srcset="" /></div>
+              <div className="csimagecont"> <img className="csimage" id="img3" src="cs3.png" alt="" srcset="" /></div>
+                
+                
+               
+            </div>
+        </div>
+    )
+}
+
+export default Home
