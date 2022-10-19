cb4cf42438d8dbb3d302a2a5996b65a70a9aae85
AdityaKakkar1402 <kakkar.adi14@gmail.com>
2022-10-19 13:14:11 +0530

    hell

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,43 +5,38 @@ const Home = () => {
         <div>
             <div className="bgimg">
                 <img className="bgimage" src="backgroundhome.png" alt="" srcset="" />
-            </div>
-            <div className="csimg">
-              <div className="csimagecont"><img className="csimage" id="img1"src="cs2.png" alt="" srcset="" /></div>
-              <div className="csimagecont"><img className="csimage" id="img2" src="cs1.png" alt="" srcset="" /></div>
-              <div className="csimagecont"> <img className="csimage" id="img3" src="cs3.png" alt="" srcset="" /></div>
-                
-                
-               
-            </div>
-            <div className="homecontent">
-                <div className="dewatextcont">
-                <div className="textimgcont">
-                <img className="textimg" src="dewatext2.png" alt="" srcset="" />
-                </div>
-                
-                <h1 className="dewades">DECENTRALISED WATCH</h1>
-
-                <div className="watchimgcont">
-                <img className="watchimg" src="watchimg.png" alt="" srcset="" />
-                </div>
-
-
-                </div>
-                
                 
             </div>
+           
 
-
-
-            <div className="csimg2">
-              <div className="csimagecont"><img className="csimage" id="img1"src="cs2.png" alt="" srcset="" /></div>
-              <div className="csimagecont"><img className="csimage" id="img2" src="cs1.png" alt="" srcset="" /></div>
-              <div className="csimagecont"> <img className="csimage" id="img3" src="cs3.png" alt="" srcset="" /></div>
-                
-                
-               
+     
+            <div className="csimagecont">
+                <img className="csimage" id="img1"src="group5.png" alt="" srcset="" />
             </div>
+       
+           
+           
+            <div className="dewatextcont">
+                   <button class="glow-on-hover" type="button">Connect Wallet</button>
+
+                   <h1 className="dewadesline1">Discover</h1>
+
+                   <div className="textimgcont">
+                      <img className="textimg" src="dewatext2.png" alt="" srcset="" />
+                   </div>
+                
+                   <h1 className="dewades">DECENTRALISED WATCH</h1>
+
+                   <div className="watchimgcont">
+                      <img className="watchimg" height="100px" src="watchimg.png" alt="" srcset="" />
+                   </div>
+
+            </div>
+         
+
+
+
+         
         </div>
     )
 }
